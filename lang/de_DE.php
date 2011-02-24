11d614928658aa7acc4eacaece95a339472bc383
Sascha Koehler <skoehler@pixeltricks.de>
2011-02-24 10:57:38 +0100

    Added language files.

---
+++ lang/de_DE.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * German (Germany) language pack
+ * @package modules: silvercart
+ */
+i18n::include_locale_file('silvercart', 'en_US');
+
+if (array_key_exists('de_DE', $lang) && is_array($lang['de_DE'])) {
+    $lang['de_DE'] = array_merge($lang['en_US'], $lang['de_DE']);
+} else {
+    $lang['de_DE'] = $lang['en_US'];
+}
+
+$lang['de_DE']['SilvercartPaymentPaypal']['NAME']       = 'Paypal';
+$lang['de_DE']['SilvercartPaymentPaypal']['TITLE']      = 'Paypal';
---
+++ lang/en_US.php
@@ -0,0 +1,5 @@
+<?php
+global $lang;
+
+$lang['en_US']['SilvercartPaymentPaypal']['NAME']       = 'Paypal';
+$lang['en_US']['SilvercartPaymentPaypal']['TITLE']      = 'Paypal';
