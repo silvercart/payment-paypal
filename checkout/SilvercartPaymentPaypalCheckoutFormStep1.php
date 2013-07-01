d3e845a38cb693a06bc7ac2f06a3773be720d330
Sebastian Diel <sdiel@pixeltricks.de>
2013-07-01 16:31:13 +0200

    MINOR: updated licencing

--- checkout/SilvercartPaymentPaypalCheckoutFormStep1.php
+++ checkout/SilvercartPaymentPaypalCheckoutFormStep1.php
@@ -1,21 +1,8 @@
 <?php
 /**
- * Copyright 2010, 2011 pixeltricks GmbH
+ * Copyright 2013 pixeltricks GmbH
  *
  * This file is part of SilverCart.
- *
- * SilverCart is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SilverCart is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with SilverCart.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package Silvercart
  * @subpackage Forms_Checkout
@@ -26,10 +13,11 @@
  *
  * @package Silvercart
  * @subpackage Forms_Checkout
- * @author Sascha Koehler <skoehler@pixeltricks.de>
- * @copyright Pixeltricks GmbH
- * @since 06.04.2011
- * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @author Sascha Koehler <skoehler@pixeltricks.de>,
+ *         Sebastian Diel <sdiel@pixeltricks.de>
+ * @since 01.07.2013
+ * @copyright 2013 pixeltricks GmbH
+ * @license see license file in modules root directory
  */
 class SilvercartPaymentPaypalCheckoutFormStep1 extends SilvercartCheckoutFormStepPaymentInit {
 
@@ -39,7 +27,6 @@ class SilvercartPaymentPaypalCheckoutFormStep1 extends SilvercartCheckoutFormSte
      * @return void
      *
      * @author Sascha Koehler <skoehler@pixeltricks.de>
-     * @copyright 2011 pixeltricks GmbH
      * @since 07.04.2011
      */
     public function preferences() {
@@ -54,7 +41,8 @@ class SilvercartPaymentPaypalCheckoutFormStep1 extends SilvercartCheckoutFormSte
      *
      * @return void
      *
-     * @author Sascha Koehler <skoehler@pixeltricks.de>, Sebastian Diel <sdiel@pixeltricks.de>
+     * @author Sascha Koehler <skoehler@pixeltricks.de>,
+     *         Sebastian Diel <sdiel@pixeltricks.de>
      * @since 05.02.2013
      */
     public function process() {
